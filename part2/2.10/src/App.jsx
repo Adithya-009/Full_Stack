5222ce57e4dd522c8b40757f44a4d5a86c26fab0
Adithya-009 <23it009adit@ug.cusat.ac.in>
2025-02-05 14:03:41 +0530

    2.10

--- part2/2.10/src/App.jsx
+++ part2/2.10/src/App.jsx
@@ -2,10 +2,10 @@ import { useState } from 'react';
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+    { name: 'Mayookha', number: '040-123456', id: 1 },
+    { name: 'Ashwin', number: '39-44-5323523', id: 2 },
+    { name: 'Adarsh', number: '12-43-234345', id: 3 },
+    { name: 'Ajay', number: '39-23-6423122', id: 4 }
   ]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
